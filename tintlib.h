04325d3137eefa0e922bfaf34a03ef4ddd6cb180
unlyx.xyz <unlyx.xyz@proton.me>
2026-05-03 10:25:01 -0300

    refactor: improve naming consistency

--- tintlib.h
+++ tintlib.h
@@ -20,22 +20,22 @@ Version: 1.2.2
 */
 
 typedef enum {
-    ESCAPE_CODE_OCTAL = 0,
-    ESCAPE_CODE_HEXADECIMAL = 2,
-} TL_EscapeCodesIndex;
-extern const char* const TL_EscapeCodes[];
+    ANSI_C0_ESC_OCTAL = 0,
+    ANSI_C0_ESC_HEXADECIMAL = 2,
+} TL_ANSIC0ESCIndex;
+extern const char* const TL_ANSIC0ESC[];
 
 typedef enum {
-    TL_GRAPHICS_MODE_RESET = 0,
-    TL_GRAPHICS_MODE_BOLD = 1,
-    TL_GRAPHICS_MODE_DIM = 2,
-    TL_GRAPHICS_MODE_ITALIC = 3,
-    TL_GRAPHICS_MODE_UNDERLINE = 4,
-    TL_GRAPHICS_MODE_BLINKING = 5,
-    TL_GRAPHICS_MODE_INVERSE = 6,
-    TL_GRAPHICS_MODE_HIDDEN = 7,
-    TL_GRAPHICS_MODE_STRIKETHROUGH = 8,
-} TL_GraphicModesIndex;
+    ANSI_C1_CSI_SGR_RESET = 0,
+    ANSI_C1_CSI_SGR_BOLD = 1,
+    ANSI_C1_CSI_SGR_DIM = 2,
+    ANSI_C1_CSI_SGR_ITALIC = 3,
+    ANSI_C1_CSI_SGR_UNDERLINE = 4,
+    ANSI_C1_CSI_SGR_BLINKING = 5,
+    ANSI_C1_CSI_SGR_INVERSE = 6,
+    ANSI_C1_CSI_SGR_HIDDEN = 7,
+    ANSI_C1_CSI_SGR_STRIKETHROUGH = 8,
+} TL_SGRDisplayAttributesIndex;
 typedef struct {
     bool bold;
     bool dim;
@@ -45,56 +45,55 @@ typedef struct {
     bool inverse;
     bool hidden;
     bool strikethrough;
-} TL_GraphicModeConfig;
-extern const char* const TL_GraphicModes[];
+} TL_SGRDisplayAttributes;
+extern const char* const TL_SGRDisplayAttributesPS[];
 
 typedef enum {
-    TL_COLOR_CODE_FG_BLACK = 0,
-    TL_COLOR_CODE_FG_RED = 1,
-    TL_COLOR_CODE_FG_GREEN = 2,
-    TL_COLOR_CODE_FG_YELLOW = 3,
-    TL_COLOR_CODE_FG_BLUE = 4,
-    TL_COLOR_CODE_FG_MAGENTA = 5,
-    TL_COLOR_CODE_FG_CYAN = 6,
-    TL_COLOR_CODE_FG_WHITE = 7,
-    TL_COLOR_CODE_FG_DEFAULT = 8,
-    TL_COLOR_CODE_RESET = 9,
-    TL_COLOR_CODE_FG_BRIGHT_BLACK = 10,
-    TL_COLOR_CODE_FG_BRIGHT_RED = 11,
-    TL_COLOR_CODE_FG_BRIGHT_GREEN = 12,
-    TL_COLOR_CODE_FG_BRIGHT_YELLOW = 13,
-    TL_COLOR_CODE_FG_BRIGHT_BLUE = 14,
-    TL_COLOR_CODE_FG_BRIGHT_MAGENTA = 15,
-    TL_COLOR_CODE_FG_BRIGHT_CYAN = 16,
-    TL_COLOR_CODE_FG_BRIGHT_WHITE = 17,
-    TL_COLOR_CODE_BG_BLACK = 18,
-    TL_COLOR_CODE_BG_RED = 19,
-    TL_COLOR_CODE_BG_GREEN = 20,
-    TL_COLOR_CODE_BG_YELLOW = 21,
-    TL_COLOR_CODE_BG_BLUE = 22,
-    TL_COLOR_CODE_BG_MAGENTA = 23,
-    TL_COLOR_CODE_BG_CYAN = 24,
-    TL_COLOR_CODE_BG_WHITE = 25,
-    TL_COLOR_CODE_BG_DEFAULT = 26,
-    TL_COLOR_CODE_BG_BRIGHT_BLACK = 27,
-    TL_COLOR_CODE_BG_BRIGHT_RED = 28,
-    TL_COLOR_CODE_BG_BRIGHT_GREEN = 29,
-    TL_COLOR_CODE_BG_BRIGHT_YELLOW = 30,
-    TL_COLOR_CODE_BG_BRIGHT_BLUE = 31,
-    TL_COLOR_CODE_BG_BRIGHT_MAGENTA = 32,
-    TL_COLOR_CODE_BG_BRIGHT_CYAN = 33,
-    TL_COLOR_CODE_BG_BRIGHT_WHITE = 34,
-} TL_IndexedColorsIndex;
+    ANSI_C1_CSI_SGR_FG_BLACK = 0,
+    ANSI_C1_CSI_SGR_FG_RED = 1,
+    ANSI_C1_CSI_SGR_FG_GREEN = 2,
+    ANSI_C1_CSI_SGR_FG_YELLOW = 3,
+    ANSI_C1_CSI_SGR_FG_BLUE = 4,
+    ANSI_C1_CSI_SGR_FG_MAGENTA = 5,
+    ANSI_C1_CSI_SGR_FG_CYAN = 6,
+    ANSI_C1_CSI_SGR_FG_WHITE = 7,
+    ANSI_C1_CSI_SGR_FG_DEFAULT = 8,
+    ANSI_C1_CSI_SGR_BG_BLACK = 9,
+    ANSI_C1_CSI_SGR_BG_RED = 10,
+    ANSI_C1_CSI_SGR_BG_GREEN = 11,
+    ANSI_C1_CSI_SGR_BG_YELLOW = 12,
+    ANSI_C1_CSI_SGR_BG_BLUE = 13,
+    ANSI_C1_CSI_SGR_BG_MAGENTA = 14,
+    ANSI_C1_CSI_SGR_BG_CYAN = 15,
+    ANSI_C1_CSI_SGR_BG_WHITE = 16,
+    ANSI_C1_CSI_SGR_BG_DEFAULT = 17,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_BLACK = 18,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_RED = 19,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_GREEN = 20,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_YELLOW = 21,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_BLUE = 22,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_MAGENTA = 23,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_CYAN = 24,
+    ANSI_C1_CSI_SGR_FG_BRIGHT_WHITE = 25,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_BLACK = 26,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_RED = 27,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_GREEN = 28,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_YELLOW = 29,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_BLUE = 30,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_MAGENTA = 31,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_CYAN = 32,
+    ANSI_C1_CSI_SGR_BG_BRIGHT_WHITE = 33,
+} TL_SGRColors3Bit4BitIndex;
 typedef struct {
-    TL_IndexedColorsIndex foreground;
-    TL_IndexedColorsIndex background;
-} TL_IndexedColorConfig;
-extern const char* const TL_IndexedColors[];
+    TL_SGRColors3Bit4BitIndex foreground;
+    TL_SGRColors3Bit4BitIndex background;
+} TL_SGRColors3Bit4Bit;
+extern const char* const TL_SGRColors3Bit4BitPS[];
 
 typedef struct {
     int foreground;
     int background;
-} TL_8BitConfig;
+} TL_SGRColors8Bit;
 
 typedef struct {
     bool enable;
@@ -106,20 +105,20 @@ typedef struct {
 typedef struct {
     TL_RGB foreground;
     TL_RGB background;
-} TL_TruecolorConfig;
+} TL_SGRColorsTruecolor;
 
 typedef enum {
-    SEQUENCE_INDEXED = 0,
-    SEQUENCE_8BIT = 1,
-    SEQUENCE_TRUECOLOR = 2,
-    SEQUENCE_JUST_GRAPHICS = 3,
+    ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES = 0,
+    ANSI_C1_CSI_SGR_3BIT4BIT = 1,
+    ANSI_C1_CSI_SGR_8BIT = 2,
+    ANSI_C1_CSI_SGR_TRUECOLOR = 3,
 } _tl_sequence_type;
 
 typedef struct {
-    TL_GraphicModeConfig graphics;
-    TL_IndexedColorConfig indexed;
-    TL_8BitConfig eight_bit;
-    TL_TruecolorConfig truecolor;
+    TL_SGRDisplayAttributes da;
+    TL_SGRColors3Bit4Bit c3bit4bit;
+    TL_SGRColors8Bit c8bit;
+    TL_SGRColorsTruecolor ctruecolor;
 } TL_Sequence;
 
 TL_Sequence TL_InitSequence(void);
@@ -132,13 +131,13 @@ TL_Sequence TL_InitSequence(void);
 
 #ifdef TINT_LIB_IMPLEMENTATION
 
-#ifndef TINT_LIB_ESCAPE_CODE
-#define TINT_LIB_ESCAPE_CODE ESCAPE_CODE_OCTAL
+#ifndef TINT_LIB_C0_ESC
+#define TINT_LIB_C0_ESC ANSI_C0_ESC_OCTAL
 #endif
 
-const char* const TL_EscapeCodes[] = {"\033", "\x1B"};
-const char* const TL_GraphicModes[] = {"0", "1", "2", "3", "4", "5", "7", "8", "9"};
-const char* const TL_IndexedColors[] = {"30", "31", "32", "33", "34", "35", "36", "37", "39", "0", "90", "91", "92", "93", "94", "95", "96", "97" "40", "41", "42", "43", "44", "45", "46", "47", "49", "100", "101", "102", "103", "104", "105", "106", "107"};
+const char* const TL_ANSIC0ESC[] = {"\033", "\x1B"};
+const char* const TL_SGRDisplayAttributesPS[] = {"0", "1", "2", "3", "4", "5", "7", "8", "9"};
+const char* const TL_SGRColors3Bit4BitPS[] = {"30", "31", "32", "33", "34", "35", "36", "37", "39", "40", "41", "42", "43", "44", "45", "46", "47", "49", "90" "91", "92", "93", "94", "95", "96", "97", "100", "101", "102", "103", "104", "105", "106", "107"};
 
 TL_Sequence TL_InitSequence(void) {
 
@@ -193,46 +192,46 @@ static inline int _tl_build_sequence(FILE *stream, _tl_sequence_type type, TL_Se
     char msg_seq[msg_seq_len];
     memset(msg_seq, 0, msg_seq_len);
 
-    strcpy(msg_seq, TL_EscapeCodes[TINT_LIB_ESCAPE_CODE]);
+    strcpy(msg_seq, TL_ANSIC0ESC[TINT_LIB_C0_ESC]);
     strcat(msg_seq, "[");
 
-    _apply_property(msg_seq, sequence.graphics.bold, TL_GraphicModes[TL_GRAPHICS_MODE_BOLD]);
-    _apply_property(msg_seq, sequence.graphics.dim, TL_GraphicModes[TL_GRAPHICS_MODE_DIM]);
-    _apply_property(msg_seq, sequence.graphics.italic, TL_GraphicModes[TL_GRAPHICS_MODE_ITALIC]);
-    _apply_property(msg_seq, sequence.graphics.underline, TL_GraphicModes[TL_GRAPHICS_MODE_UNDERLINE]);
-    _apply_property(msg_seq, sequence.graphics.blinking, TL_GraphicModes[TL_GRAPHICS_MODE_BLINKING]);
-    _apply_property(msg_seq, sequence.graphics.inverse, TL_GraphicModes[TL_GRAPHICS_MODE_INVERSE]);
-    _apply_property(msg_seq, sequence.graphics.hidden, TL_GraphicModes[TL_GRAPHICS_MODE_HIDDEN]);
-    _apply_property(msg_seq, sequence.graphics.strikethrough, TL_GraphicModes[TL_GRAPHICS_MODE_STRIKETHROUGH]);
+    _apply_property(msg_seq, sequence.da.bold, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_BOLD]);
+    _apply_property(msg_seq, sequence.da.dim, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_DIM]);
+    _apply_property(msg_seq, sequence.da.italic, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_ITALIC]);
+    _apply_property(msg_seq, sequence.da.underline, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_UNDERLINE]);
+    _apply_property(msg_seq, sequence.da.blinking, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_BLINKING]);
+    _apply_property(msg_seq, sequence.da.inverse, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_INVERSE]);
+    _apply_property(msg_seq, sequence.da.hidden, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_HIDDEN]);
+    _apply_property(msg_seq, sequence.da.strikethrough, TL_SGRDisplayAttributesPS[TL_GRAPHICS_MODE_STRIKETHROUGH]);
 
     switch(type) {
-        case SEQUENCE_INDEXED:
-            snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "%s;%s;", TL_IndexedColors[sequence.indexed.foreground], TL_IndexedColors[sequence.indexed.background]);
+        case ANSI_C1_CSI_SGR_3BIT4BIT:
+            snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "%s;%s;", TL_SGRColors3Bit4BitPS[sequence.c3bit4bit.foreground], TL_SGRColors3Bit4BitPS[sequence.c3bit4bit.background]);
             break;
-        case SEQUENCE_8BIT:
-            if(sequence.eight_bit.foreground >= 0) {
-                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "38;5;%d;", sequence.eight_bit.foreground);
+        case ANSI_C1_CSI_SGR_8BIT:
+            if(sequence.c8bit.foreground >= 0) {
+                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "38;5;%d;", sequence.c8bit.foreground);
             }
-            if(sequence.eight_bit.background >= 0) {
-                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "48;5;%d;", sequence.eight_bit.background);
-            }
-            break;
-        case SEQUENCE_TRUECOLOR:
-            if(sequence.truecolor.foreground.enable) {
-                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "38;2;%d;%d;%d;", sequence.truecolor.foreground.r, sequence.truecolor.foreground.g, sequence.truecolor.foreground.b); 
-            }
-            if(sequence.truecolor.background.enable) {
-                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "48;2;%d;%d;%d;", sequence.truecolor.background.r, sequence.truecolor.background.g, sequence.truecolor.background.b);
+            if(sequence.c8bit.background >= 0) {
+                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "48;5;%d;", sequence.c8bit.background);
             }
             break;
-        case SEQUENCE_JUST_GRAPHICS:
+        case ANSI_C1_CSI_SGR_TRUECOLOR:
+            if(sequence.ctruecolor.foreground.enable) {
+                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "38;2;%d;%d;%d;", sequence.ctruecolor.foreground.r, sequence.ctruecolor.foreground.g, sequence.ctruecolor.foreground.b); 
+            }
+            if(sequence.ctruecolor.background.enable) {
+                snprintf(msg_seq + strlen(msg_seq), sizeof(msg_seq) - strlen(msg_seq), "48;2;%d;%d;%d;", sequence.ctruecolor.background.r, sequence.ctruecolor.background.g, sequence.ctruecolor.background.b);
+            }
+            break;
+        case ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES:
             break;
     }
 
     strcat(msg_seq, "m");
 
     char msg[2048] = {0};
-    snprintf(msg, sizeof(msg), "%s%s%s[0m", msg_seq, fmt, TL_EscapeCodes[TINT_LIB_ESCAPE_CODE]);
+    snprintf(msg, sizeof(msg), "%s%s%s[0m", msg_seq, fmt, TL_ANSIC0ESC[TINT_LIB_ESCAPE_CODE]);
 
     va_list args;
     va_start(args, fmt);
@@ -243,46 +242,46 @@ static inline int _tl_build_sequence(FILE *stream, _tl_sequence_type type, TL_Se
 
 };
 
-#define TL_IndexedPrintf(sequence, ...) _tl_build_sequence(stdout, SEQUENCE_INDEXED, sequence, ##__VA_ARGS__)
-#define TL_8BitPrintf(sequence, ...) _tl_build_sequence(stdout, SEQUENCE_8BIT, sequence, ##__VA_ARGS__)
-#define TL_TruecolorPrintf(sequence, ...) _tl_build_sequence(stdout, SEQUENCE_TRUECOLOR, sequence, ##__VA_ARGS__)
-#define TL_GraphicsPrintf(sequence, ...) _tl_build_sequence(stdout, SEQUENCE_JUST_GRAPHICS, sequence, ##__VA_ARGS__)
+#define TL_printfc3bit4bit(sequence, ...) _tl_build_sequence(stdout, ANSI_C1_CSI_SGR_3BIT4BIT, sequence, ##__VA_ARGS__)
+#define TL_printfc8bit(sequence, ...) _tl_build_sequence(stdout, ANSI_C1_CSI_SGR_8BIT, sequence, ##__VA_ARGS__)
+#define TL_printfcTruecolor(sequence, ...) _tl_build_sequence(stdout, ANSI_C1_CSI_SGR_TRUECOLOR, sequence, ##__VA_ARGS__)
+#define TL_printfcDa(sequence, ...) _tl_build_sequence(stdout, ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES, sequence, ##__VA_ARGS__)
 
-#define TL_IndexedfPrintf(stream, sequence, ...) _tl_build_sequence(stream, SEQUENCE_INDEXED, sequence, ##__VA_ARGS__)
-#define TL_8BitfPrintf(stream, sequence, ...) _tl_build_sequence(stream, SEQUENCE_8BIT, sequence, ##__VA_ARGS__)
-#define TL_TruecolorfPrintf(stream, sequence, ...) _tl_build_sequence(stream, SEQUENCE_TRUECOLOR, sequence, ##__VA_ARGS__)
-#define TL_GraphicsfPrintf(stream, sequence, ...) _tl_build_sequence(stream, SEQUENCE_TRUECOLOR, sequence, ##__VA_ARGS__)
+#define TL_fprintfc3bit4bit(stream, sequence, ...) _tl_build_sequence(stream, ANSI_C1_CSI_SGR_3BIT4BIT, sequence, ##__VA_ARGS__)
+#define TL_fprintfc8bit(stream, sequence, ...) _tl_build_sequence(stream, ANSI_C1_CSI_SGR_8BIT, sequence, ##__VA_ARGS__)
+#define TL_fprintfcTruecolor(stream, sequence, ...) _tl_build_sequence(stream, ANSI_C1_CSI_SGR_TRUECOLOR, sequence, ##__VA_ARGS__)
+#define TL_fprintfcDa(stream, sequence, ...) _tl_build_sequence(stream, ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES, sequence, ##__VA_ARGS__)
 
 void _tl_copy_sequence(int type, TL_Sequence src, TL_Sequence *dest) {
     
     switch (type) {
         case -1:
-            dest->graphics = src.graphics;
-            dest->indexed = src.indexed;
-            dest->eight_bit = src.eight_bit;
-            dest->truecolor = src.truecolor;
+            dest->da = src.da;
+            dest->c3bit4bit = src.c3bit4bit;
+            dest->c8bit = src.c8bit;
+            dest->ctruecolor = src.ctruecolor;
             break;
-        case SEQUENCE_INDEXED:
-            dest->indexed = src.indexed;
+        case ANSI_C1_CSI_SGR_3BIT4BIT:
+            dest->c3bit4bit = src.c3bit4bit;
             break;
-        case SEQUENCE_8BIT:
-            dest->eight_bit = src.eight_bit;
+        case ANSI_C1_CSI_SGR_8BIT:
+            dest->c8bit = src.c8bit;
             break;
-        case SEQUENCE_TRUECOLOR:
-            dest->truecolor = src.truecolor;
+        case ANSI_C1_CSI_SGR_TRUECOLOR:
+            dest->ctruecolor = src.ctruecolor;
             break;
-        case SEQUENCE_JUST_GRAPHICS:
-            dest->graphics = src.graphics;
+        case ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES:
+            dest->da = src.da;
             break;
     } 
 
 };
 
 #define TL_CopySequence(src, dest) _tl_copy_sequence(-1, src, dest)
-#define TL_CopyIndexed(src, dest) _tl_copy_sequence(SEQUENCE_INDEXED, src, dest)
-#define TL_Copy8bit(src, dest) _tl_copy_sequence(SEQUENCE_8BIT, src, dest)
-#define TL_CopyTruecolor(src, dest) _tl_copy_sequence(SEQUENCE_TRUECOLOR, src, dest)
-#define TL_CopyGraphics(src, dest) _tl_copy_sequence(SEQUENCE_JUST_GRAPHICS, src, dest)
+#define TL_CopySequence3bit4bit(src, dest) _tl_copy_sequence(ANSI_C1_CSI_SGR_3BIT4BIT, src, dest)
+#define TL_CopySequence8bit(src, dest) _tl_copy_sequence(ANSI_C1_CSI_SGR_8BIT, src, dest)
+#define TL_CopySequenceTruecolor(src, dest) _tl_copy_sequence(ANSI_C1_CSI_SGR_TRUECOLOR, src, dest)
+#define TL_CopySequenceDa(src, dest) _tl_copy_sequence(ANSI_C1_CSI_SGR_DISPLAYATTRIBUTES, src, dest)
 
 #endif
 
